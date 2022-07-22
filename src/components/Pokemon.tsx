42a0b4d695c7ba07c7a863040b543c9cfbe16d69
Rishabh Gupta <belikerishabh@gmail.com>
2022-07-22 17:39:34 +0530

    Updated after the review

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -5,30 +5,23 @@ import { pokemonListProps } from "./Home";
 type abilitiesProps = {
   id: number;
   pokemonName: string;
-  abilities: pokemonListProps[] & {
-    description: string;
-  };
+  abilities: pokemonListProps[];
 };
 
 type effectProps = {
   effect: string;
-  language: {
-    name: string;
-    url?: string;
-  };
+  language: pokemonListProps;
 };
 
 const Pokemon = () => {
   const location = useLocation();
   const state = location.state as abilitiesProps;
-  const abilitiesArr = state.abilities;
-  const id = state.id;
-  const pokemonName = state.pokemonName;
+  const { abilities, id, pokemonName } = state;
 
   const [descriptionArr, setDescriptionArr] = useState<string[]>([]);
 
   useEffect(() => {
-    abilitiesArr.forEach((item) => {
+    abilities.forEach((item) => {
       fetch(item.url)
         .then((res) => res.json())
         .then((data) => {
@@ -52,7 +45,7 @@ const Pokemon = () => {
       <h2 className="text-3xl mt-10 font-bold">{pokemonName.toUpperCase()}</h2>
       <div className="flex flex-col w-1/2 items-left border-2 border-black border-solid rounded-md p-10 mt-10">
         <h2 className="text-red-700 text-3xl font-bold">Abilities: </h2>
-        {abilitiesArr.map((item, indx) => {
+        {abilities.map((item, indx) => {
           return (
             <div className="w-3/4 py-3" key={indx}>
               <li className="font-bold text-xl">{item.name.toUpperCase()}</li>
